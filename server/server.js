b4ef2d0e85b201ce063db6d44057ceb164935c24
ever0702 <ever0702@gmail.com>
2016-09-26 15:04:14 -0400

    'dfd'

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ let app = express();
 
 console.log('starting express app2');
 
-app.get('/', (req, res) => res.send('hello world make a change'));
+app.get('/', (req, res) => res.send('hello world make a change, this is pretty f**king awesome'));
 
 app.listen(3000, () => console.log('example app running on port 3000'));
 
