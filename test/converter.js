b0dd1e4b0c627c216b9b6a5ca95ef34d020b045f
ever0702 <ever0702@gmail.com>
2016-09-26 15:27:04 -0400

    'mocha'

---
+++ test/converter.js
@@ -0,0 +1,16 @@
+var expect = require("chai").expect;
+
+describe('color code converter', function() {
+    describe('RGB', function(argument) {
+        it('convert', function() {
+            expect(1 + 2).to.equal(3);
+        });
+    });
+
+    describe('HEX', function() {
+        it('2+3', function() {
+            expect(2 + 3).to.equal(5);
+        });
+    })
+
+});
