2675fd31e61b1a0ade632b59934d93d1bdec15fd
ever0702 <ever0702@gmail.com>
2016-09-26 14:18:55 -0400

    'ss'

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ let express = require('express');
 
 let app = express();
 
+console.log('starting express app');
+
 app.get('/', (req, res) => res.send('hello world'));
 
 app.listen(3000, () => console.log('example app running on port 3000'));
