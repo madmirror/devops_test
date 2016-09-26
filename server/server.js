8873b391859f43b924ba5a96a6a1e63cb4482f7e
ever0702 <ever0702@gmail.com>
2016-09-26 15:46:04 -0400

    'finally'

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ let app = express();
 
 console.log('starting express app2');
 
-app.get('/', (req, res) => res.send('hello world make a change, this is pretty f**king awesome, travis build added to it'));
+app.get('/', (req, res) => res.send('hello world make a change, this is pretty f**king awesome, travis build added to it, finally put it all together and it all works!!!'));
 
 app.listen(3000, () => console.log('example app running on port 3000'));
 
